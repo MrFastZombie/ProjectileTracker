1d0c69da227b3c447950dc8d6d5a73e00daa76dc
Skylar Wolf <MrFastZombie@users.noreply.github.com>
2024-03-15 22:43:40 -0500

    Didn't need the network code.

--- ProjectileTracker/ProjectileTrackerModSystem.cs
+++ ProjectileTracker/ProjectileTrackerModSystem.cs
@@ -13,18 +13,6 @@ using ProtoBuf;
 
 namespace ProjectileTracker;
 
-//The network code is based on the Wiki's example as of 3/14/2024. https://wiki.vintagestory.at/index.php?title=Modding:Network_API&oldid=169493
-[ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
-    public class NetworkApiMessage
-    {
-        public string message;
-    }
-
-[ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
-    public class NetworkApiResponse
-    {
-        public string response;
-    }
 public class ProjectileTrackerModSystem : ModSystem
 {
     #region Client
@@ -35,20 +23,6 @@ public class ProjectileTrackerModSystem : ModSystem
         clientAPI = api;
         base.Start(api);
         //api.RegisterEntityBehaviorClass("InjectProjectileTracker", typeof(InjectProjectileTracker));
-
-        clientChannel = 
-            api.Network.RegisterChannel("ProjectileTracker")
-            .RegisterMessageType(typeof(NetworkApiMessage))
-            .RegisterMessageType(typeof(NetworkApiResponse))
-            .SetMessageHandler<NetworkApiMessage>(OnServerMessage);
-    }
-
-    private void OnServerMessage(NetworkApiMessage networkMessage) {
-        clientAPI.ShowChatMessage("Received folling message from server: " + networkMessage.message);
-        clientAPI.ShowChatMessage("Sending response");
-        clientChannel.SendPacket(new NetworkApiResponse() {
-            response = "Hi server!"
-        });
     }
 
     #endregion
@@ -64,29 +38,11 @@ public class ProjectileTrackerModSystem : ModSystem
         api.RegisterEntityBehaviorClass("InjectProjectileTracker", typeof(InjectProjectileTracker)); //This behavior class contains the logic for detecting projectiles on the server side.
         //var waypoints = (api.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer).Waypoints;
         
-        serverChannel = 
-            api.Network.RegisterChannel("ProjectileTracker")
-            .RegisterMessageType(typeof(NetworkApiMessage))
-            .RegisterMessageType(typeof(NetworkApiResponse))
-            .SetMessageHandler<NetworkApiResponse>(OnClientMessage);
-
         api.ChatCommands.Create("ptpurge")
             .WithDescription("Purge all Projectile Tracker waypoints")
             .RequiresPrivilege(Privilege.chat)
             .HandleWith(new OnCommandDelegate(OnPurgeCommand));
         
-        api.ChatCommands.Create("pttest")
-            .WithDescription("Test command")
-            .RequiresPrivilege(Privilege.controlserver)
-            .HandleWith(new OnCommandDelegate(OnPttestCommand));
-        
-    }
-
-    private TextCommandResult OnPttestCommand(TextCommandCallingArgs args) {
-        serverChannel.BroadcastPacket(new NetworkApiMessage() {
-            message = "test",
-        });
-        return TextCommandResult.Success();
     }
 
     private TextCommandResult OnPurgeCommand(TextCommandCallingArgs args) {
@@ -106,14 +62,6 @@ public class ProjectileTrackerModSystem : ModSystem
         }
 
         return TextCommandResult.Success();
-    }
-
-    private void OnClientMessage(IPlayer fromPlayer, NetworkApiResponse networkMessage) {
-        serverAPI.SendMessageToGroup(
-            GlobalConstants.GeneralChatGroup,
-            "Received following message from " + fromPlayer.PlayerName + ": " + networkMessage.response,
-            EnumChatType.Notification
-        );
     }
 
     #endregion
