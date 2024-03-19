3299e929ee3fc222455a6f73720f7de89aa59cb0
Skylar Wolf <MrFastZombie@users.noreply.github.com>
2024-03-18 22:18:54 -0500

    Moved waypoint code to a new class

--- ProjectileTracker/EntityBehavior/InjectProjectileTracker.cs
+++ ProjectileTracker/EntityBehavior/InjectProjectileTracker.cs
@@ -16,9 +16,9 @@ using System.Collections.Generic;
 namespace ProjectileTracker.EntityBehavior {
     using EntityBehavior = Vintagestory.API.Common.Entities.EntityBehavior;
     public class InjectProjectileTracker : EntityBehavior {
-        private static readonly MethodInfo ResendWaypoints = typeof(WaypointMapLayer).GetMethod("ResendWaypoints", BindingFlags.NonPublic | BindingFlags.Instance);
-        private static readonly MethodInfo RebuildMapComponents = typeof(WaypointMapLayer).GetMethod("RebuildMapComponents", BindingFlags.NonPublic | BindingFlags.Instance);
         private static Dictionary<long, bool> projectileLanded = new();
+
+        private static PtWaypoint ptWaypoint = new();
 
         public InjectProjectileTracker(Entity entity) : base(entity) {
             
@@ -31,7 +31,6 @@ namespace ProjectileTracker.EntityBehavior {
         public override void OnEntitySpawn()
         {
             EntityProjectile checkArrow = entity as EntityProjectile;
-            IServerAPI api = entity.Api as IServerAPI;
             base.OnEntitySpawn();
             projectileLanded.Add(checkArrow.EntityId, false);
 
@@ -45,7 +44,8 @@ namespace ProjectileTracker.EntityBehavior {
             base.OnEntityDespawn(despawn);
             api.Logger.Log(EnumLogType.Debug, "Projectile Despawned: " + checkArrow.EntityId);
             //Remove waypoint referring to this projectile entity if it exists.
-            RemoveWaypoint(api, checkArrow);
+            //RemoveWaypoint(api, checkArrow);
+            ptWaypoint.RemoveWaypoint(api, checkArrow);
         }
 
         public override void OnGameTick(float deltaTime)
@@ -63,7 +63,8 @@ namespace ProjectileTracker.EntityBehavior {
                 if(checkArrow.ServerPos.XYZ == checkArrow.PreviousServerPos.XYZ) {
                     api.Logger.Log(EnumLogType.Debug, "Projectile" + checkArrow.EntityId + " has landed");
                     projectileLanded[checkArrow.EntityId] = true;
-                    CreateWaypoint(api, checkArrow);
+                    //CreateWaypoint(api, checkArrow);
+                    ptWaypoint.CreateWaypoint(api, checkArrow);
                 }
             }
 
@@ -73,71 +74,6 @@ namespace ProjectileTracker.EntityBehavior {
                 //Add the waypoint here.
                 CreateWaypoint(api, checkArrow);
             }*/
-        }
-
-        private System.Collections.Generic.List<Waypoint> getWaypoints(ICoreServerAPI api) {
-            var waypoints = (api.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer).Waypoints;
-            return waypoints;
-        }
-
-        private static void CreateWaypoint(ICoreServerAPI api, EntityProjectile p) {
-            var maplayer = api.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer;
-            var player = (p.FiredBy as EntityPlayer).Player as IServerPlayer;
-            Waypoint newWaypoint = new() {
-                Position = p.ServerPos.XYZ,
-                Title = "Projectile " + p.EntityId,
-                Pinned = false,
-                Icon = "circle",
-                Color = 0,
-                OwningPlayerUid = player.PlayerUID
-            };
-
-            maplayer.AddWaypoint(newWaypoint, (p.FiredBy as EntityPlayer).Player as IServerPlayer);
-        }
-
-        private void RemoveWaypoint(ICoreServerAPI api, EntityProjectile p) {
-            if(p.FiredBy == null) {
-                RemoveOrphanedWaypoint(api, p);
-                return; //When the world is reloaded, FiredBy becoems null on saved projectile entiities.
-            }
-
-            var maplayer = api.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer;
-            var waypoints = getWaypoints(api);
-            var player = (p.FiredBy as EntityPlayer).Player as IServerPlayer;
-            foreach (Waypoint waypoint in waypoints.ToList().Where(w => w.OwningPlayerUid == player.PlayerUID))
-            {
-                if(waypoint.Title == "Projectile " + p.EntityId) {
-                    if(player == null) {
-                        api.Logger.Log(EnumLogType.Error, "Projectile " + p.EntityId + " had a waypoint for player " + waypoint.OwningPlayerUid + " but that player could not be retrieved. If this happened during a world load, this can be ignored!");
-                        continue;
-                    }
-
-                    waypoints.Remove(waypoint);
-                    ResendWaypoints.Invoke(maplayer, new Object[] { player });
-                    RebuildMapComponents.Invoke(maplayer, null);
-                }
-            }
-        }
-
-        //Until https://github.com/anegostudios/VintageStory-Issues/issues/3723 is fixed, this method will remove waypoints for orphaned projectiles.
-        private void RemoveOrphanedWaypoint(ICoreServerAPI api, EntityProjectile p) {
-            var maplayer = api.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer;
-            var waypoints = getWaypoints(api);
-            
-            foreach (Waypoint waypoint in waypoints.ToList())
-            {
-                if(waypoint.Title == "Projectile " + p.EntityId) {
-                    var player = api.World.PlayerByUid(waypoint.OwningPlayerUid);
-                    if(player == null) {
-                        api.Logger.Log(EnumLogType.Error, "Orphaned projectile " + p.EntityId + " had a waypoint for player " + waypoint.OwningPlayerUid + " but that player could not be retrieved.");
-                        continue;
-                    }
-
-                    waypoints.Remove(waypoint);
-                    ResendWaypoints.Invoke(maplayer, new Object[] {player as IServerPlayer});
-                    RebuildMapComponents.Invoke(maplayer, null);
-                }
-            }
         }
     }
 }
---
+++ ProjectileTracker/PtWaypoint.cs
@@ -0,0 +1,87 @@
+using ProjectileTracker.EntityBehavior;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Config;
+using Vintagestory.API;
+using Vintagestory.API.Server;
+using Vintagestory.GameContent;
+using Vintagestory.API.MathTools;
+using System.Linq;
+using System;
+using System.Reflection;
+using ProtoBuf;
+using Vintagestory.API.Util;
+using System.Security.Cryptography.X509Certificates;
+using Vintagestory.ServerMods;
+using Newtonsoft.Json;
+
+namespace ProjectileTracker;
+class PtWaypoint
+{
+    public PtWaypoint() { }
+
+    private static readonly MethodInfo ResendWaypoints = typeof(WaypointMapLayer).GetMethod("ResendWaypoints", BindingFlags.NonPublic | BindingFlags.Instance);
+    private static readonly MethodInfo RebuildMapComponents = typeof(WaypointMapLayer).GetMethod("RebuildMapComponents", BindingFlags.NonPublic | BindingFlags.Instance);
+    
+    public void CreateWaypoint(ICoreServerAPI api, EntityProjectile p) {
+        var maplayer = api.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer;
+            var player = (p.FiredBy as EntityPlayer).Player as IServerPlayer;
+            Waypoint newWaypoint = new() {
+                Position = p.ServerPos.XYZ,
+                Title = "Projectile " + p.EntityId,
+                Pinned = false,
+                Icon = "ptarrow",
+                Color = ColorUtil.Hex2Int("#b55aed"),
+                OwningPlayerUid = player.PlayerUID
+            };
+
+            maplayer.AddWaypoint(newWaypoint, (p.FiredBy as EntityPlayer).Player as IServerPlayer);
+    }
+    
+    public void RemoveWaypoint(ICoreServerAPI api, EntityProjectile p) {
+        if(p.FiredBy == null) {
+                RemoveOrphanedWaypoint(api, p);
+                return; //When the world is reloaded, FiredBy becoems null on saved projectile entiities.
+        }
+
+        var maplayer = api.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer;
+        var waypoints = getWaypoints(api);
+        var player = (p.FiredBy as EntityPlayer).Player as IServerPlayer;
+        foreach (Waypoint waypoint in waypoints.ToList().Where(w => w.OwningPlayerUid == player.PlayerUID))
+        {
+            if(waypoint.Title == "Projectile " + p.EntityId) {
+                if(player == null) {
+                    api.Logger.Log(EnumLogType.Error, "Projectile " + p.EntityId + " had a waypoint for player " + waypoint.OwningPlayerUid + " but that player could not be retrieved. If this happened during a world load, this can be ignored!");
+                    continue;
+                }
+
+                waypoints.Remove(waypoint);
+                ResendWaypoints.Invoke(maplayer, new Object[] { player });
+                RebuildMapComponents.Invoke(maplayer, null);
+            }
+        }
+    } //End of RemoveWaypoint()
+
+    //Until https://github.com/anegostudios/VintageStory-Issues/issues/3723 is fixed, this method will remove waypoints for orphaned projectiles.
+    private void RemoveOrphanedWaypoint(ICoreServerAPI api, EntityProjectile p) {
+        var maplayer = api.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer;
+        var waypoints = getWaypoints(api);
+        
+        foreach (Waypoint waypoint in waypoints.ToList())
+        {
+            if(waypoint.Title == "Projectile " + p.EntityId) {
+                var player = api.World.PlayerByUid(waypoint.OwningPlayerUid);
+                if(player == null) {
+                    api.Logger.Log(EnumLogType.Error, "Orphaned projectile " + p.EntityId + " had a waypoint for player " + waypoint.OwningPlayerUid + " but that player could not be retrieved.");
+                    continue;
+                }
+            }
+        }
+    } //End of RemoveOrphanedWaypoint()
+
+    private System.Collections.Generic.List<Waypoint> getWaypoints(ICoreServerAPI api) {
+        var waypoints = (api.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer).Waypoints;
+        return waypoints;
+    }
+} //End of PtWaypoint
