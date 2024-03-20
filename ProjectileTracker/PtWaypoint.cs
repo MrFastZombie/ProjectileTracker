4f69f293eaddb59716cff0a644d864301a3f6792
Skylar Wolf <MrFastZombie@users.noreply.github.com>
2024-03-20 15:55:55 -0500

    documentation

--- ProjectileTracker/PtWaypoint.cs
+++ ProjectileTracker/PtWaypoint.cs
@@ -16,23 +16,38 @@ class PtWaypoint
     private static readonly MethodInfo ResendWaypoints = typeof(WaypointMapLayer).GetMethod("ResendWaypoints", BindingFlags.NonPublic | BindingFlags.Instance);
     private static readonly MethodInfo RebuildMapComponents = typeof(WaypointMapLayer).GetMethod("RebuildMapComponents", BindingFlags.NonPublic | BindingFlags.Instance);
     
+    // ------------------------------------------------------------------------------------Public Functions------------------------------------------------------------------------------------
+    
+    /// <summary>
+    /// Creates a projectile waypoint.
+    /// </summary>
+    /// <param name="api">Server's api.</param>
+    /// <param name="p">The projectile to create a waypoint for.</param>
     public void CreateWaypoint(ICoreServerAPI api, EntityProjectile p) {
         var maplayer = api.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer;
         var player = (p.FiredBy as EntityPlayer).Player as IServerPlayer;
         Ptconfig playerConfig = ProjectileTrackerModSystem.clientConfigs[player.PlayerUID];
-        if(playerConfig == null || !playerConfig.EnableProjectileTracker) return;
+
+        if(playerConfig == null || !playerConfig.EnableProjectileTracker) return; //Don't create a waypoint if either the player does not have the mod or if they disabled the mod in the config
+
         Waypoint newWaypoint = new() {
             Position = p.ServerPos.XYZ,
             Title = "Projectile " + p.EntityId,
             Pinned = false,
             Icon = playerConfig.icon,
-            Color = System.Drawing.ColorTranslator.FromHtml(playerConfig.color).ToArgb(),
+            Color = System.Drawing.ColorTranslator.FromHtml(playerConfig.color).ToArgb(), //Accepts HTML hex color codes.
             OwningPlayerUid = player.PlayerUID
         };
 
         maplayer.AddWaypoint(newWaypoint, (p.FiredBy as EntityPlayer).Player as IServerPlayer);
-    }
+    } // End of CreateWaypoint()
     
+    /// <summary>
+    /// Removes a projectile waypoint.
+    /// </summary>
+    /// <param name="api">Server's api</param>
+    /// <param name="p">Projectile to remove a waypoint for.</param>
+    /// <param name="forcestore">Debug parameter that allows you to force a waypoint to be stored instead of being sent directly to the player.</param>
     public void RemoveWaypoint(ICoreServerAPI api, EntityProjectile p, bool forcestore = false) {
         if(p.FiredBy == null) {
                 RemoveOrphanedWaypoint(api, p);
@@ -42,7 +57,8 @@ class PtWaypoint
         var maplayer = api.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer;
         var waypoints = getWaypoints(api);
         var player = (p.FiredBy as EntityPlayer).Player as IServerPlayer;
-        foreach (Waypoint waypoint in waypoints.ToList().Where(w => w.OwningPlayerUid == player.PlayerUID))
+
+        foreach (Waypoint waypoint in waypoints.ToList().Where(w => w.OwningPlayerUid == player.PlayerUID)) //For every waypoint the player owns, check if it relates the the projectile and remove it if so.
         {
             if(waypoint.Title == "Projectile " + p.EntityId) {
                 if(player == null || forcestore) {
@@ -58,7 +74,54 @@ class PtWaypoint
         }
     } //End of RemoveWaypoint()
 
-    //Until https://github.com/anegostudios/VintageStory-Issues/issues/3723 is fixed, this method will remove waypoints for orphaned projectiles.
+    /// <summary>
+    /// Retrives the list of waypoints from the server's map layer.
+    /// </summary>
+    /// <param name="api">Server's api.</param>
+    /// <returns></returns>
+    public System.Collections.Generic.List<Waypoint> getWaypoints(ICoreServerAPI api) {
+        var waypoints = (api.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer).Waypoints;
+        return waypoints;
+    } //End of getWaypoints()
+
+    /// <summary>
+    /// Removes all stored waypoints for a player and sends the updates to the player.
+    /// </summary>
+    /// <param name="playerUID">UID of the player.</param>
+    /// <param name="serverAPI">Server's API.</param>
+    public void ProcessStoredWaypoints(string playerUID, ICoreServerAPI serverAPI) {
+        if(!ProjectileTrackerModSystem.pendingWaypoints.ContainsKey(playerUID)) return;
+        List<Waypoint> storedWaypoints = ProjectileTrackerModSystem.pendingWaypoints[playerUID].ToList();
+        var waypoints = getWaypoints(serverAPI);
+
+        var maplayer = serverAPI.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer;
+
+        foreach (Waypoint waypoint in storedWaypoints)
+        {
+            if(waypoint.Title.StartsWith("Projectile ")) {
+                waypoints.Remove(waypoint);
+            }
+
+            try
+            {
+                ResendWaypoints.Invoke(maplayer, new Object[] {serverAPI.World.PlayerByUid(playerUID) as IServerPlayer});
+                RebuildMapComponents.Invoke(maplayer, null);
+                ProjectileTrackerModSystem.pendingWaypoints.Remove(playerUID);
+            }
+            catch (System.Exception) //I reckon this might happen if the player crashes on connect.
+            {
+                serverAPI.Logger.Log(EnumLogType.Error, "Projectile Tracker failed to send waypoints for player " + playerUID + " to the client. Stored waypoint updates have not been cleared.");
+            }
+        }
+    } //End of ProcessStoredWaypoints()
+
+    // ------------------------------------------------------------------------------------Helper Functions------------------------------------------------------------------------------------
+
+    /// <summary>
+    /// Until https://github.com/anegostudios/VintageStory-Issues/issues/3723 is fixed, this method will remove waypoints for orphaned projectiles.
+    /// </summary>
+    /// <param name="api">Server's api.</param>
+    /// <param name="p">The Projectile that may have a waypoint.</param>
     private void RemoveOrphanedWaypoint(ICoreServerAPI api, EntityProjectile p) {
         var maplayer = api.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer;
         var waypoints = getWaypoints(api);
@@ -80,38 +143,14 @@ class PtWaypoint
         }
     } //End of RemoveOrphanedWaypoint()
 
-    public System.Collections.Generic.List<Waypoint> getWaypoints(ICoreServerAPI api) {
-        var waypoints = (api.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer).Waypoints;
-        return waypoints;
-    }
-
-    private void StoreWaypoint(string playerUID, Waypoint wp) {
+    /// <summary>
+    /// Stores a waypoint update so that it can be sent to the client later. For when the client that owns a waypoint is not online.
+    /// </summary>
+    /// <param name="playerUID">UID of the player.</param>
+    /// <param name="wp">Waypoint that needs to be stored.</param>
+    private static void StoreWaypoint(string playerUID, Waypoint wp) {
         if(!ProjectileTrackerModSystem.pendingWaypoints.ContainsKey(playerUID)) ProjectileTrackerModSystem.pendingWaypoints[playerUID] = new();
         ProjectileTrackerModSystem.pendingWaypoints[playerUID].Add(wp);
-    }
+    } //End of StoreWaypoint()
 
-    public void ProcessStoredWaypoints(string playerUID, ICoreServerAPI serverAPI) {
-        if(!ProjectileTrackerModSystem.pendingWaypoints.ContainsKey(playerUID)) return;
-        List<Waypoint> storedWaypoints = ProjectileTrackerModSystem.pendingWaypoints[playerUID].ToList();
-        var waypoints = getWaypoints(serverAPI);
-
-        var maplayer = serverAPI.ModLoader.GetModSystem<WorldMapManager>().MapLayers.FirstOrDefault(ml => ml is WaypointMapLayer) as WaypointMapLayer;
-        foreach (Waypoint waypoint in storedWaypoints)
-        {
-            if(waypoint.Title.StartsWith("Projectile ")) {
-                waypoints.Remove(waypoint);
-            }
-
-            try
-            {
-                ResendWaypoints.Invoke(maplayer, new Object[] {serverAPI.World.PlayerByUid(playerUID) as IServerPlayer});
-                RebuildMapComponents.Invoke(maplayer, null);
-                ProjectileTrackerModSystem.pendingWaypoints.Remove(playerUID);
-            }
-            catch (System.Exception) //I reckon this might happen if the player crashes on connect.
-            {
-                serverAPI.Logger.Log(EnumLogType.Error, "Projectile Tracker failed to send waypoints for player " + playerUID + " to the client. Stored waypoint updates have not been cleared.");
-            }
-        }
-    }
 } //End of PtWaypoint
